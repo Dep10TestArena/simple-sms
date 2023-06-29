b90c2bc14ddb3c3c09034f34ddd5c4928c93f20e
Thilina Dilshan <pathiranagedilshan@gmail.com>
2023-06-29 19:26:37 +0530

    Setup Dependencies and Database

---
+++ back-end-spring-boot-sms/src/main/java/lk/ijse/dep10/backendspringbootsms/BackEndSpringBootSmsApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.backendspringbootsms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndSpringBootSmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndSpringBootSmsApplication.class, args);
+	}
+
+}
---
+++ back-end-spring-boot-sms/src/test/java/lk/ijse/dep10/backendspringbootsms/BackEndSpringBootSmsApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ijse.dep10.backendspringbootsms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndSpringBootSmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
