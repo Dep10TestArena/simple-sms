f6d273c0620670af0f412eb977c96a0b9a794b1b
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-06-29 19:41:25 +0530

    Add CourseHTTPController.ts

---
+++ backend-nodejs/src/main.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+import {router as CourseHTTPController} from "./api/CourseHTTPController";
+//import cors from 'cors';
+
+
+const app =express();
+
+//app.use(cors());
+app.use("/api/v1/courses",CourseHTTPController)
+
+app.listen(8081, () => console.log("Server has been created"));
