d40a087c3fe8f2e4d2e46065d9f2bd30c7af7e73
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-06-29 22:05:18 +0530

    Completed the Web services

--- backend-nodejs/src/api/CourseHTTPController.ts
+++ backend-nodejs/src/api/CourseHTTPController.ts
@@ -6,12 +6,37 @@ export const router = express.Router();
 
 //console.log(dataSource);
 
-router.get('/', (req, res) => {
-    res.send("<h1>Get All Books</h1>");
+type Course = {
+    id: string,
+    description: string,
+    duration: string
+}
+
+router.get('/', async (req, res) => {
+    const result = await dataSource.query('SELECT * FROM course')
+    console.log(result)
+    res.json(result);
+    //res.sendStatus(result.affectedRows ? 204 : 404);
 });
-router.post('/:id', (req, res) => {
-    res.send("<h1>Save a book</h1>");
+router.post('/', async (req, res) => {
+    const course =  req.body as Course;
+    if (!course.id?.trim()) {
+        res.sendStatus(400);
+        return;
+    }
+    if (!course.description?.trim()) {
+        res.sendStatus(400);
+        return;
+    }
+    if (!course.duration?.trim()) {
+        res.sendStatus(400);
+        return;
+    }
+    const result=await dataSource.query('INSERT INTO course (id, description, duration) VALUES (?,?,?)',[course.id,course.description,course.duration]);
+    res.sendStatus(result.affectedRows ? 200 : 404);
 });
-router.delete('/:id', (req, res) => {
-    res.send("<h1>Delete a book</h1>");
+router.delete('/:id', async (req, res) => {
+
+    const result = await dataSource.query('DELETE FROM course WHERE id=?',[req.params.id])
+    res.sendStatus(result.affectedRows ? 204 : 404);
 });
--- backend-nodejs/src/main.ts
+++ backend-nodejs/src/main.ts
@@ -1,10 +1,11 @@
-import express from 'express';
+import express, {json} from 'express';
 import {router as CourseHTTPController} from "./api/CourseHTTPController";
 import cors from 'cors';
 
 
 const app =express();
 
+app.use(json())
 app.use(cors());
 app.use("/api/v1/courses",CourseHTTPController)
 
