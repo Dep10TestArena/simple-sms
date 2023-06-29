79ab258f478aad579bc2b2b4e8d08e384d25b9ea
Kushan Weerarathne <kushanweerarathne14@gmail.com>
2023-06-29 20:08:23 +0530

    Expose following web services
    GET /api/v1/courses
    POST /api/v1/courses
    DELETE /api/v1/courses/{courseId}

--- backend-nodejs/src/api/CourseHTTPController.ts
+++ backend-nodejs/src/api/CourseHTTPController.ts
@@ -2,3 +2,13 @@ import express from "express";
 
 
 export const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send("<h1>Get All Books</h1>");
+});
+router.post('/:id', (req, res) => {
+    res.send("<h1>Save a book</h1>");
+});
+router.delete('/:id', (req, res) => {
+    res.send("<h1>Delete a book</h1>");
+});
